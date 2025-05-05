d779c2cef1f26e0255061270faffe0d9166be591
oliv2673 <oliver.nicas2303@gmail.com>
2025-05-05 14:16:49 +0200

    updated profile

--- js/profile.js
+++ js/profile.js
@@ -1,21 +1,29 @@
 import { BASE_URL } from "./info.js";
 
 const showProfile = async () => {
-  
   let user_id = sessionStorage.getItem("user_id");
-  console.log(user_token)
-  const response = await fetch(`${BASE_URL}/users/${user_id}`);
+  let user_token = sessionStorage.getItem("user_token");
+
+  console.log('User ID:', user_id);
+  console.log('Token:', user_token);
+
+  const response = await fetch(`${BASE_URL}/users/${user_id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Session-Token': user_token
+    }
+  });
+
+  if (!response.ok) {
+    console.error(`Error ${response.status}: Unable to fetch profile`);
+    return;
+  }
+
   const data = await response.json();
   const profile = document.createDocumentFragment();
-  
 
-  document.querySelector('#firstName').value = data.first_name
-
-  
-
-
-
+  document.querySelector('#firstName').value = data.first_name;
   document.querySelector('#form-profile').append(profile);
 };
-console.log(showProfile());
+
 showProfile();
