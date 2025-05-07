ec24a835dbf261816cd820947d883a7483383cbb
oliv2673 <oliver.nicas2303@gmail.com>
2025-05-07 13:10:22 +0200

    nav changes

---
+++ js/nav.js
@@ -0,0 +1,32 @@
+
+let user_id = sessionStorage.getItem("user_id");
+let user_token = sessionStorage.getItem("user_token");
+const adminID = '2679'
+
+
+if (user_id !== null) {
+  if (user_id === adminID) {
+      document.querySelector('#user-logged')?.classList.add('hidden');
+      document.querySelector('#not-logged')?.classList.add('hidden');
+      document.querySelector('#admin-logged')?.classList.remove('hidden');
+  } else {
+      document.querySelector('#user-logged')?.classList.remove('hidden');
+      document.querySelector('#admin-logged')?.classList.add('hidden');
+      document.querySelector('#not-logged')?.classList.add('hidden');
+  }
+} else {
+  document.querySelector('#user-logged')?.classList.add('hidden');
+  document.querySelector('#admin-logged')?.classList.add('hidden');
+  document.querySelector('#not-logged')?.classList.remove('hidden');
+}
+ 
+// Logging out implies removing user data from sessionStorage
+document.querySelector('.btn-logout')?.addEventListener('click', (e) => {
+    e.preventDefault();
+ 
+    sessionStorage.removeItem('user_id');
+    sessionStorage.removeItem('user_token');
+ 
+    window.location.href = 'index.html';
+});
+console.log(".btn-logout")
