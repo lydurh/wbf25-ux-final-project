3fadb78376c901c354f29d554b338bb0ca8c2e43
Frederikke <frny0001@stud.kea.dk>
2025-05-08 10:55:27 +0200

    profile functionality

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,6 @@
 import { BASE_URL } from "./info.js";
+import { APIerrorResponse} from './handle-errors.js';
+import {header} from './api.js';
 
 const showProfile = async () => {
   let user_id = sessionStorage.getItem("user_id");
@@ -21,9 +23,64 @@ const showProfile = async () => {
 
   const data = await response.json();
   const profile = document.createDocumentFragment();
-
+  console.log(data);
+  document.querySelector('#membershipDate').innerText = data.membership_date;
   document.querySelector('#firstName').value = data.first_name;
+  document.querySelector('#lastName').value = data.last_name;
+  document.querySelector('#email').value = data.email;
+  document.querySelector('#date').value = data.birth_date;
+  document.querySelector('#address').value = data.address;
+  document.querySelector('#phoneNumber').value = data.phone_number;
   document.querySelector('#form-profile').append(profile);
 };
+document.addEventListener('DOMContentLoaded', function(){
+  const form = document.getElementById("form-profile");
+  const formInputs = form.querySelectorAll('input');
+  formInputs.forEach(input => {
+    input.disabled = true;
+  })
+})
 
 showProfile();
+
+const editProfile = () => {
+let user_id = sessionStorage.getItem("user_id");
+let user_token = sessionStorage.getItem("user_token");
+const form = document.getElementById("form-profile");
+const editBtn = document.getElementById("updateUserBtn");
+const saveBtn = document.getElementById("saveUserBtn");
+const formInputs = form.querySelectorAll('input');
+
+editBtn.addEventListener('click', (e)=> {
+  e.preventDefault();
+  formInputs.forEach(input => {
+    input.disabled = false;
+  })
+  editBtn.classList.add("hidden")
+  saveBtn.classList.remove("hidden");
+})
+form.addEventListener('submit', (e)=> {
+  e.preventDefault();
+  fetch(`${BASE_URL}/users/${user_id}`,{
+    method: "PUT",
+    body: URLSearchParams,
+    headers : header
+  })
+  .then((response) => response.json())
+  .then((data) => {
+      if (data.status === "ok") {
+          showSuccessMessage("Changes saved!", "success", "main");
+          form(false); // disable form after successfull submit
+      } else {
+      }
+  })
+  .catch(APIerrorResponse);
+
+  formInputs.forEach(input => {
+    input.disabled = true;
+  })
+  saveBtn.classList.add("hidden")
+  editBtn.classList.remove("hidden");
+})
+};
+editProfile();
