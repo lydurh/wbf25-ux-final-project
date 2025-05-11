ae57e884f78e7e43349ce90c3c752a21e5f850e7
oliv2673 <48830541+oliv2673@users.noreply.github.com>
2025-05-11 17:14:20 +0200

    only admin can see adminpage

--- js/admin.js
+++ js/admin.js
@@ -1,82 +1,88 @@
 import { BASE_URL } from "./info.js";
 import { header } from './api.js';
+const user_id = sessionStorage.getItem("user_id");
+const adminID = '2679';
+
+if (user_id != adminID) {
+  document.querySelector('section').remove();
+  document.querySelector('h1').innerText = "You're not allowed on this page"
+} else {
+
+  document.querySelector('#form_publisher').addEventListener('submit', (e) => {
+    e.preventDefault();
 
 
-document.querySelector('#form_publisher').addEventListener('submit',(e)=> {
-  e.preventDefault();
+    let user_id = sessionStorage.getItem("user_id");
+    const publisherName = e.target.publisherName.value.trim();
 
-
-  let user_id = sessionStorage.getItem("user_id");
-  const publisherName = e.target.publisherName.value.trim();
-
-  const params = new URLSearchParams();
-  params.append('name', publisherName);
+    const params = new URLSearchParams();
+    params.append('name', publisherName);
 
 
 
 
-  fetch(`${BASE_URL}/admin/${user_id}/publishers`, {
-  method: "POST",
-  body: params,
-  headers: header
+    fetch(`${BASE_URL}/admin/${user_id}/publishers`, {
+      method: "POST",
+      body: params,
+      headers: header
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+
+      });
   })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data)
-    
-});
-})
 
 
 
-const showAuthors = async () => {
+  const showAuthors = async () => {
 
     const response = await fetch(`${BASE_URL}/authors`);
     const data = await response.json();
-    
-    const author = data.author || data.results || data; 
-    
+
+    const author = data.author || data.results || data;
+
     const fragment = document.createDocumentFragment();
-    
+
     author.forEach(author => {
       const option = document.querySelector('#authorTemplate').content.cloneNode(true);
       option.querySelector('.optionAuthorId').innerText = author.author_name;
       option.querySelector('.optionAuthorId').value = author.author_id;
-      
-  
 
-    fragment.append(option);
+
+
+      fragment.append(option);
     });
-    
+
     document.querySelector('#authorId').append(fragment);
-};
+  };
 
-showAuthors();
+  showAuthors();
 
-const showPublishers = async () => {
+  const showPublishers = async () => {
 
     const response = await fetch(`${BASE_URL}/publishers`);
     const data = await response.json();
-    
-    const publisher = data.publisher || data.results || data; 
-    
+
+    const publisher = data.publisher || data.results || data;
+
     const fragment = document.createDocumentFragment();
-    
+
     publisher.forEach(publisher => {
       const option = document.querySelector('#publisherTemplate').content.cloneNode(true);
       option.querySelector('.optionPublisherId').innerText = publisher.publisher_name;
       option.querySelector('.optionPublisherId').value = publisher.publisher_id;
-  
 
-    fragment.append(option);
+
+      fragment.append(option);
     });
-    
+
     document.querySelector('#publisherId').append(fragment);
-};
+  };
 
-showPublishers();
+  showPublishers();
 
-document.querySelector('#form_book').addEventListener('submit',(e)=> {
+  document.querySelector('#form_book').addEventListener('submit', (e) => {
     e.preventDefault();
 
 
@@ -94,25 +100,25 @@ document.querySelector('#form_book').addEventListener('submit',(e)=> {
 
 
     fetch(`${BASE_URL}/admin/${user_id}/books`, {
-    method: "POST",
-    body: params,
-    headers: header
+      method: "POST",
+      body: params,
+      headers: header
     })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-      
-});
-})
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
 
-document.querySelector('#form_authors').addEventListener('submit',(e)=> {
+      });
+  })
+
+  document.querySelector('#form_authors').addEventListener('submit', (e) => {
     e.preventDefault();
 
 
     let user_id = sessionStorage.getItem("user_id");
     const authorFirstName = e.target.authorFirstName.value.trim();
     const authorLastName = e.target.authorLastName.value.trim();
-  
+
 
     const params = new URLSearchParams();
     params.append('first_name', authorFirstName);
@@ -121,14 +127,14 @@ document.querySelector('#form_authors').addEventListener('submit',(e)=> {
 
 
     fetch(`${BASE_URL}/admin/${user_id}/authors`, {
-    method: "POST",
-    body: params,
-    headers: header
+      method: "POST",
+      body: params,
+      headers: header
     })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-      
-});
-})
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
 
+      });
+  })
+}
